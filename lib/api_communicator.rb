079c2be5b210a757d563f804d9b8a822f48998a8
Damon Collins <damon.c.p.collins@gmail.com>
2018-12-03 17:49:48 -0500

    done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,24 +3,33 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character_name)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
+  url = 'http://www.swapi.co/api/people/'
+  response = response_hash(url)
+  char_film_urls = character_films(response, character_name)
+  char_film_urls[character_name].map { |film_url| response_hash(film_url) }
+end #end of get_character_movies_from_api
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+def character_films(hash, character_name)
+  films = {}
+  hash["results"].map { |data| films[character_name] = data["films"] }
+  films
+end
+
+def response_hash(url)
+  response_string = RestClient.get(url)
+  JSON.parse(response_string)
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
-end
+  puts "-----------------------------------------"
+  films.each do |data|
+    puts "Star Wars: #{data["title"]}."
+    puts "Episode: #{data["episode_id"]}."
+    puts "Director: #{data["director"]}."
+    puts "Description #{data["opening_crawl"][0...150]}..."
+    puts "-----------------------------------------"
+  end
+end #end of print_movies
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
@@ -28,6 +37,8 @@ def show_character_movies(character)
 end
 
 ## BONUS
+
+
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
